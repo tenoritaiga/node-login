215a735636cb5b639cd0ce47adc20815d43bd514
tenoritaiga <ringbearer91@gmail.com>
2013-11-27 19:26:19 -0500

    Grab usernames from local cookie for chat names, center icon on homepage

--- app/public/js/views/chat.js
+++ app/public/js/views/chat.js
@@ -5,12 +5,39 @@ window.onload = function() {
     var field = document.getElementById("inputMessage");
     var sendButton = document.getElementById("inputSend");
     var content = document.getElementById("chatbox");
-    //var name = document.getElementById("name");
-    var name = "Tony";
+
     //placeOverlay();
     //generateKeypair();
     //var pubkey = generateKeypair(name,'supersecretpassphrase');
     //removeOverlay();
+
+    function getCookie(c_name)
+    {
+        var c_value = document.cookie;
+        var c_start = c_value.indexOf(" " + c_name + "=");
+        if (c_start == -1)
+        {
+            c_start = c_value.indexOf(c_name + "=");
+        }
+        if (c_start == -1)
+        {
+            c_value = null;
+        }
+        else
+        {
+            c_start = c_value.indexOf("=", c_start) + 1;
+            var c_end = c_value.indexOf(";", c_start);
+            if (c_end == -1)
+            {
+                c_end = c_value.length;
+            }
+            c_value = unescape(c_value.substring(c_start,c_end));
+        }
+        return c_value;
+    }
+
+    //console.log("Username: " + getCookie("user"));
+    var name = getCookie("user");
 
     socket.on('message', function (data) {
 	//console.log("got message from server: "+data);
